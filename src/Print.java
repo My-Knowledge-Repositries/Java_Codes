1c4b14ba7092dce5226bd89044303e2c8211f4fd
oshadaera68 <mindartlk010@gmail.com>
2022-11-14 20:26:18 +0530

    updated content

---
+++ src/Print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Print {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input name:- ");
+        String s1 = scan.next();
+        System.out.print("Input Address:- ");
+        String s2 = scan.next();
+        System.out.print(s1 + "\n" + s2);
+    }
+}
