f29feb11e0b8199621d21590e9256361f5a486b2
oshadaera68 <mindartlk010@gmail.com>
2022-11-14 20:07:32 +0530

    added

---
+++ src/Dec.java
@@ -0,0 +1,9 @@
+public class Dec {
+//    static void main(String args[]){
+//        System.out.println("sdjs");
+//    }
+
+//     public static void main(String args){
+//         System.out.println("jksjd");
+//     }
+}
