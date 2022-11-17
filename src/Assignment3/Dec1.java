2259f63636561320c09a99165cf8b267bee0f504
oshadaera68 <mindartlk010@gmail.com>
2022-11-17 19:41:05 +0530

    updated content

---
+++ src/Assignment3/Dec1.java
@@ -0,0 +1,14 @@
+package Assignment3;
+
+public class Dec1 {
+    public static void main(String[] args) {
+        byte b1=100; // legal 1
+//        byte b2=128; //illegal
+        byte b3=-128; //legal 3
+        byte b4=0; //legal 4
+        short s1=100; // legal 5
+//        short s2=32768; // illegal
+        short s3=32767; //legal 7
+        short s4=-32768; //legal 8
+    }
+}
